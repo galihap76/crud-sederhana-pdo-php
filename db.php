3da24deb4b062edd03cec60f1c29b83e73afe226
Galih Anggoro Prasetya <83481679+galihap76@users.noreply.github.com>
2023-06-16 22:37:20 +0700

    Update db.php

--- db.php
+++ db.php
@@ -8,12 +8,8 @@ $password = "";
 $conn = new PDO("mysql:host=$servername;dbname=db_mahasiswa", $username, $password);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+// Tutup koneksi database
 function tutupKoneksi($stmt = null, $conn = null)
-{
-    // cek apakah parameter statement dan koneksi tidak null
-    if ($stmt !== null && $conn !== null) {
-        // tutup statement dan koneksi database
-        $stmt->closeCursor();
-        $conn = null;
-    }
+{ 
+    $conn = null;
 }
