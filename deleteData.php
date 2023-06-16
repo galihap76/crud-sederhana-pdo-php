d5af8fefbefa3b0d20e10489a91ab447e8bd874a
Galih Anggoro Prasetya <83481679+galihap76@users.noreply.github.com>
2023-06-16 22:37:57 +0700

    Update deleteData.php

--- deleteData.php
+++ deleteData.php
@@ -8,7 +8,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stmt = $conn->prepare("DELETE FROM tbl_mahasiswa WHERE id_mahasiswa = :id_mahasiswa"); // Menyiapkan pernyataan SQL untuk menghapus data dari tabel 'tbl_mahasiswa' berdasarkan 'id_mahasiswa'
     $stmt->bindParam(':id_mahasiswa', $id_mahasiswa); // Mengikat nilai 'id_mahasiswa' ke parameter-placeholder ':id_mahasiswa'
     $stmt->execute(); // Menjalankan pernyataan SQL untuk menghapus data dari tabel 'tbl_mahasiswa' berdasarkan 'id_mahasiswa'
-    tutupKoneksi($stmt, $conn); // Menutup koneksi database dengan memanggil fungsi 'tutupKoneksi'
+    tutupKoneksi($conn); // Menutup koneksi database dengan memanggil fungsi 'tutupKoneksi'
 
 } else {
     header('HTTP/1.1 404 Not found'); // Mengirimkan header respons HTTP 404 jika permintaan bukan metode POST
